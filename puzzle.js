a191b224486026be6690908ceba92a3a927a4848
sreedevi <sreedevisobhana@gmail.com>
2022-02-21 13:22:00 +0530

    js modified1

--- puzzle.js
+++ puzzle.js
@@ -1,89 +1,19 @@
 $(function () {
-    $(".puzzle").draggable({
+
+    var parent = $("#playArea");
+    var child = $(".puzzle");
+
+    child.draggable({
         grid: [200, 200],
-        containment: "element",
-        revert: false
-        // iframeFix: true,
+        containment: parent,
+        helper: "clone",
+        // revert: false
+        // // iframeFix: true,
         // opacity: 1
 
-    });
+        startfunction
 
-    $(function () {
-        var
     })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // $(".puzzle").css({ opacity: 0 });
-
-})
-//--------------------------------------------
-// $(document).ready(function () {
-
-//     var puzzle = $(".puzzle");
-//     var playArea = $("#playArea");
-
-//     puzzle.draggable({
-//         containment: playArea,
-//         helper: "clone",
-
-//         start: function () {
-//             $(this).css({
-//                 opacity: 0
-//             });
-
-//             $(".puzzle").css("z-index", "0");
-//         },
-
-//         stop: function () {
-//             $(this).css({
-//                 opacity: 1
-//             });
-//         }
-//     });
-
-//     puzzle.droppable({
-//         accept: puzzle,
-
-//         drop: function (event, ui) {
-//             var draggable = ui.draggable;
-//             var droppable = $(this);
-//             var dragPos = draggable.position();
-//             var dropPos = droppable.position();
-
-//             draggable.css({
-//                 left: dropPos.left + "px",
-//                 top: dropPos.top + "px",
-//                 "z-index": 20
-//             });
-
-//             droppable.css("z-index", 10).animate({
-//                 left: dragPos.left,
-//                 top: dragPos.top
-//             });
-//         }
-//     });
-
-// });
