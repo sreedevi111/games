5e4f30e1caf5537d52aaefd3fe5469b76cc64baa
sreedevi <sreedevisobhana@gmail.com>
2022-02-22 01:17:55 +0530

    final product

--- puzzle.js
+++ puzzle.js
@@ -2,53 +2,75 @@ function rules() {
     alert('Rearrange the pieces so that you get a sample image. \nThe steps taken are counted');
 }
 
-a = [];
-expected = ["p1", "p3", "p2", "p4"];
+
+$(document).ready(function () {
+
+    var puzzle = $(".puzzle");
+    var mainCanvas = $("#playArea");
 
 
-$(function () {
+    expected = ["1", "2", "3", "4"];
 
-    var parent = $("#playArea");
-    var child = $(".puzzle");
+    puzzle.draggable({
+        containment: mainCanvas,
+        helper: "clone",
+        // grid: [200, 200],
+        start: function (event, ui) {
+            console.log("start");
+            $(this).css({
+                opacity: 0
+            });
+            $(".puzzle").css("z-index", "0");
+        },
 
-    child.draggable({
-        grid: [200, 200],
-        containment: parent,
-        // helper: "clone"
-        // revert: false
-        // // iframeFix: true,
-        // opacity: 1
-
-        // start: function () {
-        //     alert("listen")
-        //     $(this).css({
-        //         opacity: 1
-        //     });
-        //     $(".puzzle").css("z-index", 0)
-        // },
         stop: function (event, ui) {
-
-            $('#playArea .container').children().each(function (index) {
-                a.push(this.className.split(' ')[1])
-                if (JSON.stringify(a.slice(Math.max(a.length - 4, 1))) === JSON.stringify(expected)) {
-                    alert("You Won!!!! Hurray!!!!");
+            $(this).css({
+                opacity: 1
+            });
+            flag = true;
+            setTimeout(function () {
+                for (let i = 0; i < 4; i++) {
+                    var togetArr = mainCanvas.children()[i]
+                    console.log($(togetArr).attr("key"), i)
+                    console.log(typeof ($(togetArr).attr("key")), typeof (i.toString()))
+                    if ($(togetArr).attr("key") !== i.toString()) {
+                        flag = false;
+                        break;
+                    }
                 }
-
-                console.log(a.slice(Math.max(a.length - 4, 1)), expected)
-            })
+                if (flag == true) {
+                    alert("atlast you won!!!!")
+                }
+            }, 800)
         }
+    }
+    );
+    puzzle.droppable({
+        accept: puzzle,
+        drop: function (event, ui) {
+            var draggable = ui.draggable;
+            var droppable = $(this);
+            var dragPos = draggable.position();
+            var dropPos = droppable.position();
 
+            draggable.css({
+                left: dropPos.left + "px",
+                top: dropPos.top + "px",
+                "z-index": 20
+            });
+            var dragKeyAttr = draggable.attr("key");
 
+            var dropKeyAttr = droppable.attr("key");
+
+            draggable.attr("key", dropKeyAttr);
+            droppable.attr("key", dragKeyAttr);
+
+            droppable.css("z-index", 10).animate({
+                left: dragPos.left,
+                top: dragPos.top
+
+            });
+        }
     });
-    // $(".puzzle").sortable({
-    //     start: function (event, ui) { }
-    // })
-    child.sortable();
-
-
-
-
 });
-
-
 
