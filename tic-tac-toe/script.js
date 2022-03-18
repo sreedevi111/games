6328aaa4a6aae07d4d661479361b1922b2d91702
sreedevi <sreedevisobhana@gmail.com>
2022-03-18 23:41:31 +0530

    checking turns

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,8 +1,18 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+
 const cellElements =  document.querySelectorAll('[data-cell]')
+let circleTurn
+
 cellElements.forEach(cell=>{
     cell.addEventListener('click', handleClick, {once : true})
 })
 
 function handleClick(e){
-    
+    const cell = e.target
+    const currentCLASS = circleTurn ? CIRCLE_CLASS : X_CLASS
+    //place mark
+    //check for win
+    // check for draw
+    // switch turns
 }
